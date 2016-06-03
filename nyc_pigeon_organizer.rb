d3770c5f0ade506f38d444dc814970f2264f7651
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-03 08:44:14 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'Pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do | cgl_key, cgl_hash |
+    cgl_hash.each do |detail_key, names_array|
+      names_array.each do |name|
+
+        if new_hash.has_key?(name)
+          new_hash[name][cgl_key] << detail_key.to_s
+        else
+          new_hash[name] = {:color => [], :gender => [], :lives => []}
+          new_hash[name][cgl_key] << detail_key.to_s
+        end
+        
+      end
+    end
+  end
+
+  new_hash
 end
