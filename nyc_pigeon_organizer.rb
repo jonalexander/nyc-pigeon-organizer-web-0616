982a7a41461709989b8df6b99ce496fa2d1cdf03
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-07 13:12:45 -0400

    Done. Utilized each_with_object method."

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,40 @@
 require 'Pry'
 
+# New Code
+
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-
-  data.each do | cgl_key, cgl_hash |
-    cgl_hash.each do |detail_key, names_array|
-      names_array.each do |name|
-
-        if new_hash.has_key?(name)
-          new_hash[name][cgl_key] << detail_key.to_s
-        else
-          new_hash[name] = {:color => [], :gender => [], :lives => []}
-          new_hash[name][cgl_key] << detail_key.to_s
-        end
-        
+  data.each_with_object({}) do |(attribute, details_hash), result|
+    details_hash.each do |detail, names|
+      names.each do |name|
+        #binding.pry
+        result[name] ||= {}
+        result[name][attribute] ||= []
+        result[name][attribute] << detail.to_s
       end
     end
   end
-
-  new_hash
 end
+
+
+
+# Original Code
+
+# def nyc_pigeon_organizer(data)
+#   new_hash = {}
+
+#   data.each do | attribute, details |
+#     details.each do |detail, names|
+#       names.each do |name|
+
+#         if new_hash.has_key?(name)
+#           new_hash[name][attribute] << detail.to_s
+#         else
+#           new_hash[name] = {:color => [], :gender => [], :lives => []}
+#           new_hash[name][attribute] << detail.to_s
+#         end
+#       end
+#     end
+#   end
+
+#   new_hash
+# end
